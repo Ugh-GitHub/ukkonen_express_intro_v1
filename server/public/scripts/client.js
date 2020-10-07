4cc4610f7932ff208a87071a8b48523154a011c9
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2020-10-07 15:46:59 -0500

    updated notes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,20 @@
 console.log("hello from JS");
+
+$(document).ready(onReady);
+
+function onReady() {
+    console.log('hello from jquery');
+    // perfrom GET request
+    getRandomQuote();
+}
+
+function getRandomQuote(){
+    console.log('get the quote');
+    $.ajax({
+        method: 'GET',
+        url: '/randomQuote'
+    }).then(function(response){
+        console.log('response', response);
+    });
+
+}
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 // Require express - gives us a function
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // Create an instance of express by calling the function 
 // returned above - gives us an object
@@ -11,13 +12,14 @@ const quotesData = require ('./modules/quotes.js');
 // express static file serving - public is the folder name
 app.use(express.static('server/public'));
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 let index = 0;
 
 app.get('/quotes', (req, res) => {
     console.log('hi from get request');
     res.send(quotesData);
-})
+});
 
 app.get('/randomQuote', (req, res) => {
     let randomNumber = getRandomInt(quotesData.length);
@@ -27,6 +29,13 @@ app.get('/randomQuote', (req, res) => {
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
+
+app.post('/quotes', (req, res) => {
+    console.log('hello from post', req.body);
+    quotesData.push(req.body);
+    console.log(quotesData);
+    res.sendStatus(200);
+});
 
 app.listen(port, () => {
     console.log("Up and running on port: ", port);
