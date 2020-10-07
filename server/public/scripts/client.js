87bf4dad7eb6912bfcdb6c560b7622e37378b743
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2020-10-07 16:04:09 -0500

    appended to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -15,6 +15,17 @@ function getRandomQuote(){
         url: '/randomQuote'
     }).then(function(response){
         console.log('response', response);
+        appendToDom(response);
     });
 
+}
+
+function appendToDom(quote) {
+    // take the response from the server
+    // append it to #output so it shows up on the DOM
+    console.log('in append to dom with',quote);
+    $('#output').append(`
+    <i>"${quote.quote}"</i>
+    <p> by ${quote.author}</p>
+    `);
 }
